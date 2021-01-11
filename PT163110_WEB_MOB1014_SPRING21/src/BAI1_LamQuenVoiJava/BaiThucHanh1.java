8917340bef5f31e0e68469490a7009f14fe1a002
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-01-11 08:48:20 +0700

    dungna

---
+++ PT163110_WEB_MOB1014_SPRING21/src/BAI1_LamQuenVoiJava/BaiThucHanh1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI1_LamQuenVoiJava;
+
+/**
+ *
+ * @author Dungna29
+ */
+public class BaiThucHanh1 {
+    /*
+        Bài 1: Chương trình cho phép nhập họ và tên sinh viên, năm sinh, mã sinh viên 
+        từ bàn phím và xuất ra định dạng sau: 
+	“Chào bạn <<họ và tên>> | <<năm sinh>>  | <<mã sinh viên>> 
+        đến với chương trình java đầu tiên.”
+        
+        Bài 2: Viết chương trình máy tính bỏ túi cộng 5 số nguyên sau đó in kết quả
+        và những số đó ra màn hình.
+        so1 + so2 + so3 + so4 + so5 = kết quả.   
+    */
+}
