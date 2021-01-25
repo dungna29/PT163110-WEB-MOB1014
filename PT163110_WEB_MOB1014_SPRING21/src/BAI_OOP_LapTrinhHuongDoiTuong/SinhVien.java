3abfe562a0e3f4ce4f606cfe0352d8da7239441e
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-01-25 08:56:18 +0700

    1

---
+++ PT163110_WEB_MOB1014_SPRING21/src/BAI_OOP_LapTrinhHuongDoiTuong/SinhVien.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI_OOP_LapTrinhHuongDoiTuong;
+
+/**
+ *
+ * @author Dungna29
+ */
+//Tên đối tượng bắt buộc là danh từ và viết hoa chữ cái đầu của từ
+public class SinhVien {
+    
+    //Phần 1: Liệt kê tất cả các thuộc tính của đối tượng
+    String ten;
+    String sdt;
+    String queQuan;
+    int namsinh;
+    
+    //Phần 2: Hàm tạo Contructor
+    
+    //Phần 3: Getter và Setter
+    
+    //Phần 4: Phương thức
+    void inRaManHinh(){
+        System.out.printf("Chào mừng %s + %s \n",ten,sdt);
+    }
+}
